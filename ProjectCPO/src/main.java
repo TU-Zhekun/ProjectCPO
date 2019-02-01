cce63ed70edd80f13208a0edce047e8a97fcde39
TU_Zhekun <TU_Zhekun@users.noreply.github.com>
2019-02-01 15:13:56 +0100

    update

--- ProjectCPO/src/main.java
+++ ProjectCPO/src/main.java
@@ -7,7 +7,7 @@ public class main {
     public static void main(String[] args) {
         GUIWelcome guiWelcome = new GUIWelcome();
         guiWelcome.showGUI();
-        GUIGlobalInfo guiGlobalInfo = new GUIGlobalInfo("null_1","00:00","00\u00B0C", "00%");
+        GUIGlobalInfo guiGlobalInfo = new GUIGlobalInfo("null_1","00:00","00\u00B0C", "000%");
         guiGlobalInfo.setTextLabelInfoModule("nullMod");
         guiGlobalInfo.showGUI();
     }
