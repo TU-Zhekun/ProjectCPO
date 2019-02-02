c713a1b64c986566f0ac666f3dc3ffcb1ac21735
TU_Zhekun <TU_Zhekun@users.noreply.github.com>
2019-02-03 00:10:40 +0100

    update lib

--- ProjectCPO/src/main.java
+++ ProjectCPO/src/main.java
@@ -1,22 +1,9 @@
-/**
- * sssss
- *
- *
- */
 public class main {
     public static void main(String[] args) {
-        /*
         GUIWelcome guiWelcome = new GUIWelcome();
         guiWelcome.showGUI();
-        GUIGlobalInfo guiGlobalInfo =
-                new GUIGlobalInfo("Mod_null","00:00","00\u00B0C", "000%");
+        GUIGlobalInfo guiGlobalInfo = new GUIGlobalInfo("null_1","00:00","00\u00B0C", "00%");
+        guiGlobalInfo.setTextLabelInfoModule("nullMod");
         guiGlobalInfo.showGUI();
-        GUISignIn guiSignIn = new GUISignIn();
-        guiSignIn.showGUI();
-    */
-        ClientHttp clt = new ClientHttp();
-        clt.se_connecter("172.030.200.003");
-        //clt.se_connecter("192.168.000.002");
-
     }
 }
