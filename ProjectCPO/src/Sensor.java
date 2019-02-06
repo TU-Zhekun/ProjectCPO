3bcb5932920e21ea3e16fb51569c41b5447cd4de
TU_Zhekun <TU_Zhekun@users.noreply.github.com>
2019-02-06 17:30:25 +0100

    update TCP connection

--- ProjectCPO/src/Sensor.java
+++ ProjectCPO/src/Sensor.java
@@ -1,21 +1,83 @@
+import Communication.ClientTcp;
+
+import java.io.IOError;
+import java.io.IOException;
+
 /**
  * class Sensor
  * 传感器 类
  * 定义 传感器 方法
  */
-/*
 
 public class Sensor {
+    int temperature;
+    int humidity;
+    ClientTcp client;
+
     public Sensor() {
     }
-    //方法 获取温度
+
+    /**
+     * 方法 获取温度  待处理！！！
+     * ---byte of java are signed, cannot be bigger than 127
+     * ---must be converted to unsigned byte
+     */
     public int getTemp() {
+        byte[] tab = {0, 1, 0, 0, 0, 6, 8, 3, 0, 100, 0, 1};
+        byte[] tab2;
+        int rul;
+        ClientTcp client = new ClientTcp();
+        try {
+            client.se_connecter("169.254.211.21", 502);
+        } catch (Exception e) {
+            System.out.println("getTemperature Failed");
+            System.out.println(e.toString());
+        } finally {
+            //
+            client.envoyer_octets(tab);
+            client.recevoir_octets();
+            tab2 = client.getTrame_reponse();
+            rul = tab2[9]*255 +tab2[10];
+//            for (int i = 0; i < tab2.length; i++){
+//                System.out.println(tab2[i]);
+//            }
+            client.se_deconnecter();
+        }
+        return rul;
 
+
+        //方法 获取湿度
+//        public int getHumi () {
+//
     }
-    //方法 获取湿度
-    public int getHumi() {
 
+    /**
+     * 方法 获取湿度  待处理！！！
+     * ---byte of java are signed, cannot be bigger than 127
+     * ---must be converted to unsigned byte
+     */
+    public int getHumi() {
+        byte[] tab = {0, 1, 0, 0, 0, 6, 8, 3, 0, 101, 0, 1};
+        byte[] tab2;
+        int rul;
+        ClientTcp client = new ClientTcp();
+        try {
+            client.se_connecter("169.254.211.21", 502);
+        } catch (Exception e) {
+            System.out.println("getTemperature Failed");
+            System.out.println(e.toString());
+        } finally {
+            //
+            client.envoyer_octets(tab);
+            client.recevoir_octets();
+            tab2 = client.getTrame_reponse();
+            rul = tab2[9]*255 +tab2[10];
+//            for (int i = 0; i < tab2.length; i++){
+//                System.out.println(tab2[i]);
+//            }
+            client.se_deconnecter();
+        }
+        return rul;
     }
 }
 
-*/
--- ProjectCPO/src/main.java
+++ ProjectCPO/src/main.java
@@ -1,3 +1,5 @@
+import Communication.ClientTcp;
+
 public class main {
     public static void main(String[] args) {
         /*
@@ -10,11 +12,25 @@ public class main {
         //GUISignIn guiSignIn = new GUISignIn();
         //guiSignIn.showGUI();
 
-        ClientHttp http = new ClientHttp();
-        http.se_connecter("172.030.200.003");
-
-        GUISetting guiSetting = new GUISetting();
-        guiSetting.showGUI();
-
+//        ClientHttp http = new ClientHttp();
+//        http.se_connecter("169.254.211.21");
+//
+//        GUISetting guiSetting = new GUISetting();
+//        guiSetting.showGUI();
+//        ClientTcp client = new ClientTcp();
+//        if (client.se_connecter("169.254.211.21", 502)) {
+//            System.out.println("ok");
+//            byte[] tab = {0, 1, 0, 0, 0, 6, 8, 3, 0, 100, 0, 2};//
+//            client.envoyer_octets(tab);
+//            client.recevoir_octets();
+//            byte[] tab2 = client.getTrame_reponse();
+//            for (int i = 0; i < tab2.length; i++)
+//                System.out.println(tab2[i]);
+//            client.se_deconnecter();
+//        } else System.out.println("pas ok");
+        Sensor sensor1 = new Sensor();
+        System.out.println(sensor1.getTemp());
+        System.out.println("############################");
+        System.out.println(sensor1.getHumi());
     }
 }
