7894c06e260c24a316e422a7726629e06cecf4b5
MD.550 <47259573+787447950@users.noreply.github.com>
2019-02-06 17:29:07 +0100

    updateGUI

--- ProjectCPO/src/main.java
+++ ProjectCPO/src/main.java
@@ -1,17 +1,15 @@
 public class main {
     public static void main(String[] args) {
-        /*
-        GUIWelcome guiWelcome = new GUIWelcome();
-        guiWelcome.showGUI();
-        GUIGlobalInfo guiGlobalInfo = new GUIGlobalInfo("null_1","00:00","00\u00B0C", "00%");
-        guiGlobalInfo.setTextLabelInfoModule("nullMod");
-        guiGlobalInfo.showGUI();
-        */
-        //GUISignIn guiSignIn = new GUISignIn();
-        //guiSignIn.showGUI();
+//        GUIWelcome guiWelcome = new GUIWelcome();
+//        guiWelcome.showGUI();
+//        GUIGlobalInfo guiGlobalInfo = new GUIGlobalInfo("null_1","00:00","00\u00B0C", "00%","14 JUL 2089");
+//        guiGlobalInfo.setTextLabelInfoModule("nullMod");
+//        guiGlobalInfo.showGUI();
+//        GUISignIn guiSignIn = new GUISignIn();
+//        guiSignIn.showGUI();
 
-        ClientHttp http = new ClientHttp();
-        http.se_connecter("172.030.200.003");
+//        ClientHttp http = new ClientHttp();
+//        http.se_connecter("172.030.200.003");
 
         GUISetting guiSetting = new GUISetting();
         guiSetting.showGUI();
